eaed296b49d40972f202e6e0070fcb33d544e00c
Swagat Nayak <msndipapal@gmail.com>
2021-12-09 21:58:12 +0530

    added one more code to the set

--- src/beginner/NumberMirror.java
+++ src/beginner/NumberMirror.java
@@ -1,10 +1,28 @@
 /**
  * Dekho bhai seedhi baat agar tumse yeh bhi nhi ho rha then aap is field ko hi chhod do kindly RIP
- * [translation]
+ * [translation] If you can't even do this then RIP mah guy just leave this field
  */
 
 package beginner;
+import java.util.*;
 
 public class NumberMirror {
-
+    public static void main(String[] args){
+        System.out.println("Bruh enter your fking number.");
+        int num = new Scanner(System.in).nextInt();
+        System.out.println("Get yo fking number back on yo face mah guy "+num+"\nNow that's a ton of damage.");
+    }
 }
+
+/**
+ * Code for the actual codechef website
+ *
+ import java.util.*;
+
+ class NumberMirror {
+    public static void main(String[] args){
+        int num = new Scanner(System.in).nextInt();
+        System.out.println(num);
+    }
+ }
+ */
